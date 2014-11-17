15f58db06d8ca4886b5bafd179bcb8ac9e00b796
gfpastorek <gfpastorek@gmail.com>
2014-11-17 13:19:17 -0600

    fixed server, broadcast works now

--- src/java/Simulator/Server.java
+++ src/java/Simulator/Server.java
@@ -10,24 +10,27 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+
 @ServerEndpoint("/server")
 public class Server {
 
-    DataFeed dataFeed;
-    HashMap<String, Session> sessions = new HashMap<String, Session>();
-
+    private static DataFeed dataFeed;
+    private static HashMap<String, Session> sessions = new HashMap<String, Session>();
+    
     @OnOpen
     public void onOpen(Session session) {
-        System.out.println(session.getId() + " has opened a connection");
         try {
             session.getBasicRemote().sendText("message|Connection Established");
             sessions.put(session.getId(), session);
+            System.out.println(session.getId() + " has opened a connection");
         } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
 
     public void sendToAll(String msg) {
+        
+        System.out.println(sessions.size());
         
         for (Entry<String, Session> e : sessions.entrySet()) {
             try{
@@ -50,7 +53,6 @@ public class Server {
     @OnMessage
     public void onMessage(String message, Session session) {
         System.out.println("Message from " + session.getId() + ": " + message);
-
         String[] msgList = message.split("\\|");
         if (msgList[0].equals("message")) 
         {
