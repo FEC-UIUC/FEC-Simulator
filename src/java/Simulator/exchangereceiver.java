6a2434308474d00bdf81f3f92b99bb099c32a852
Thibaut Xiong <thibaut.xiong@gmail.com>
2014-12-26 03:56:13 -0600

    Exchange side receiver

--- src/java/Simulator/exchangereceiver.java
+++ src/java/Simulator/exchangereceiver.java
@@ -1,18 +1,11 @@
 package java.Simulator;
 
-public class exchangereceiver(){
+public class ExchangeReceiver(){
 
 /**
  *
  * @author Thibaut Xiong
  */
-
-
-	/***
-	public void OnMessage(Order order){
-		processOrder(order.Price, order.Qty, order.Account);
-	}
-	***/
 
 	public void OnMessage(String message){
 		// get the order type
@@ -29,60 +22,16 @@ public class exchangereceiver(){
 		switch (messagetype){
 			case 1: messagetype == "order";
 				    Order order = parseOrderMessage(message);
-				    processOrder(order);
+				    //have the order object, process it on the exchange side
 		}
 
 	}
 
-	// order|userID|sym|price|qty|side|type|orderID
 	public Order parseOrderMessage(String ordermessage){
-		String field = "";
-		String userID = "";
-		String sym = "";
-		Long price;
-		Long qty;
-		int side;
-		int type;
-		String orderID = "";
-
-		int delimiter = 0;
-		for(int i = 0; i < ordermessage.length(); i++){
-			if (ordermessage[i] == '|'){
-				delimiter ++;
-				if (delimiter == 1){
-					// do nothing
-				}
-				else if (delimiter == 2){
-					userID = field;
-				}
-				else if (delimiter == 3){
-					sym = field;
-				}
-				else if (delimiter == 4){
-					price = Long.parseLong(field);
-				}
-				else if (delimiter == 5){
-					qty = Long.parseLong(field);
-				}
-				else if (delimiter == 6){
-					side = Integer.toString(field);
-				}
-				else if (delimiter == 7){
-					type = Integer.toString(field);
-				}
-				field = "";
-			}
-
-			else{
-				field += ordermessage[i];
-				if (i == ordermessage.length()-1){
-					orderID = field;
-				}
-			}
-		}
-
-		Order order = new Order(userID, sym, price, qty, side, type, orderID);
+		HashMap<String, String> ordermap = MessageFormatter.parse(ordermessage);
+		Order order = new Order(ordermap.get(userID), ordermap.get(sym), ordermap.get(price), ordermap.get(qty), ordermap.get(side), ordermap.get(type), ordermap.get(orderID));
 		return order;
 	}
+
 
 }
