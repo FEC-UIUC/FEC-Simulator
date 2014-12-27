f2dda3b9fd776ec0093a6dd87cf2d29388e93dc1
Thibaut Xiong <thibaut.xiong@gmail.com>
2014-12-27 01:48:10 -0600

    beginning order book implementation...

--- src/java/Simulator/OrderBook.java
+++ src/java/Simulator/OrderBook.java
@@ -1,20 +1,21 @@
-package java.Simulator;
+package Simulator;
+import java.util.HashMap;
 
 /**
  * Orderbook for one security
  * 
  */
 
-public class OrderBook(){
-	private final String security
+public class OrderBook{
+	private final String security;
 	public OrderBook(String security){
-		this.security = security;
-		HashMap<double, int> buySide = new HashMap<double, int>;
-		HashMap<double, int> sellSide = new HashMap<double, int>;
+            this.security = security;
+            HashMap<Double, Integer> buySide = new HashMap<Double, Integer>();
+            HashMap<Double, Integer> sellSide = new HashMap<Double, Integer>();
 	}
 
-	public getSecurity(){
-		return security;
+	public String getSecurity(){
+		return this.security;
 	}
 
 }
