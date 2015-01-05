8d5a99f8f7c2a1dd4e7209969126c3fe4e389240
gfpastorek <gfpastorek@gmail.com>
2015-01-05 14:01:35 -0600

    .

--- web/js/security_chart.js
+++ web/js/security_chart.js
@@ -1,75 +1,3 @@
-<<<<<<< HEAD
-
-var stockchart;
-    
-function makeChart() {	
-	Highcharts.setOptions({
-        global : {
-            useUTC : false
-        }
-    });
-
-    // Create the chart
-    stockchart = $('#chart-area').highcharts('StockChart', {
-
-        rangeSelector: {
-            buttons: [{
-                count: 1,
-                type: 'minute',
-                text: '1M'
-            }, {
-                count: 5,
-                type: 'minute',
-                text: '5M'
-            }, {
-                type: 'all',
-                text: 'All'
-            }],
-            inputEnabled: false,
-            selected: 0
-        },
-
-        title : {
-            text : 'Live Data'
-        },
-                
-        yAxis: [{
-                labels: {
-                    align: 'right',
-                    x: -3
-                },
-                title: {
-                    text: 'Bid/Ask Spread'
-                },
-                height: '60%',
-                lineWidth: 2
-            }, {
-                labels: {
-                    align: 'right',
-                    x: -3
-                },
-                title: {
-                    text: 'Volume'
-                },
-                top: '65%',
-                height: '35%',
-                offset: 0,
-                lineWidth: 2
-        }],
-            
-        series : [{
-            name : 'Live data',
-            type : 'line',
-            data : []
-        },{
-            type: 'column',
-            name: 'Volume',
-            data: [],
-            yAxis: 1
-        }]
-    }).highcharts();
-	
-=======
 
 var stockchart;
     
@@ -146,5 +74,4 @@ function makeChart() {
         }]
     }).highcharts();
 	
->>>>>>> 57dbd26655d3f50701c0bf4ce51fda57f917261d
 } 
