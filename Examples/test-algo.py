bcd0e321ecd8948963a5ad47c7d4d66ab6c1707e
gfpastorek <gfpastorek@gmail.com>
2015-01-16 13:31:35 -0600

    Update test-algo.py

--- Examples/test-algo.py
+++ Examples/test-algo.py
@@ -10,20 +10,20 @@ parameters = {
 
 def initialize(globals):
     # Keep track of the current month.
-    globals.currentMonth = None
+    globals['currentMonth'] = None
 
     # The current stock being held
-    globals.currentStock = None
+    globals['currentStock'] = None
     
     # The next stock that needs to get purchased (once the sell order
     # on the current stock is filled
-    globals.nextStock = None
+    globals['nextStock'] = None
 
 '''
   The main proccessing function.  This is called and passed data
 '''
 def handle_data(globals, data, orders):
-	for s in securities:
+	for s in data:
 		oID = order(s, randint(-10, 10), price=randint(80,120), order_type='limit')
 		print "OrderID = " + str(oID)
 		
